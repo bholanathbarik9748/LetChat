d0ea40b8008d931b67ed3fbb2ad0270627cf11e7
Bholanath Barik <bholanathbarik9748@gmail.com>
2021-09-20 01:17:35 +0530

    crete PhonenoActivity and just start OTP activity

---
+++ app/src/main/java/com/example/letchat/PhoneNoActivity.java
@@ -0,0 +1,14 @@
+package com.example.letchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneNoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_no);
+    }
+}
